c9eacd16821aa108bebfdf5f93e722b8456badb3
Luis Marsiglia <marsiglia.business@gmail.com>
2022-11-08 03:36:39 -0400

    chore: add robots integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config'
-
-// https://astro.build/config
 import tailwind from '@astrojs/tailwind'
 import solidJs from '@astrojs/solid-js'
+import robotsTxt from 'astro-robots-txt'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), solidJs()]
+	site: 'https://pwdg.vercel.app',
+	integrations: [tailwind(), solidJs(), robotsTxt()]
 })
